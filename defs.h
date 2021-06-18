b4416e5564ddb5f9f7086a3b6b19fe09a6cdb14b
Deepesh Nimma <deepnimma@gmail.com>
2021-06-18 18:24:58 +0530

    fixes

--- defs.h
+++ defs.h
@@ -1,8 +1,8 @@
 #ifndef DEFS_H
 #define DEFS_H
 
-#include "stdio.h"
 #include "stdlib.h"
+#include "stdio.h"
 
 // #define DEBUG
 
@@ -11,15 +11,14 @@
 #ifndef DEBUG
 #define ASSERT(n)
 #else
-#define ASSERT(n)                     \
-  if (!(n)) {                         \
-    printf("%s - Failed", #n);        \
-    printf("On %s ", __DATE__);       \
-    printf("At %s ", __TIME__);       \
-    printf("In File %s ", __FILE__);  \
-    printf("At Line %d\n", __LINE__); \
-    exit(1);                          \
-  }
+#define ASSERT(n) \
+if(!(n)) { \
+printf("%s - Failed",#n); \
+printf("On %s ",__DATE__); \
+printf("At %s ",__TIME__); \
+printf("In File %s ",__FILE__); \
+printf("At Line %d\n",__LINE__); \
+exit(1);}
 #endif
 
 typedef unsigned long long U64;
@@ -31,104 +30,26 @@ typedef unsigned long long U64;
 #define MAXPOSITIONMOVES 256
 #define MAXDEPTH 64
 
-#define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+#define START_FEN  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 #define INFINITE 30000
 #define ISMATE (INFINITE - MAXDEPTH)
 
-enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
-enum {
-  FILE_A,
-  FILE_B,
-  FILE_C,
-  FILE_D,
-  FILE_E,
-  FILE_F,
-  FILE_G,
-  FILE_H,
-  FILE_NONE
-};
-enum {
-  RANK_1,
-  RANK_2,
-  RANK_3,
-  RANK_4,
-  RANK_5,
-  RANK_6,
-  RANK_7,
-  RANK_8,
-  RANK_NONE
-};
+enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK  };
+enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
+enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
 
 enum { WHITE, BLACK, BOTH };
 enum { UCIMODE, XBOARDMODE, CONSOLEMODE };
 enum {
-  A1 = 21,
-  B1,
-  C1,
-  D1,
-  E1,
-  F1,
-  G1,
-  H1,
-  A2 = 31,
-  B2,
-  C2,
-  D2,
-  E2,
-  F2,
-  G2,
-  H2,
-  A3 = 41,
-  B3,
-  C3,
-  D3,
-  E3,
-  F3,
-  G3,
-  H3,
-  A4 = 51,
-  B4,
-  C4,
-  D4,
-  E4,
-  F4,
-  G4,
-  H4,
-  A5 = 61,
-  B5,
-  C5,
-  D5,
-  E5,
-  F5,
-  G5,
-  H5,
-  A6 = 71,
-  B6,
-  C6,
-  D6,
-  E6,
-  F6,
-  G6,
-  H6,
-  A7 = 81,
-  B7,
-  C7,
-  D7,
-  E7,
-  F7,
-  G7,
-  H7,
-  A8 = 91,
-  B8,
-  C8,
-  D8,
-  E8,
-  F8,
-  G8,
-  H8,
-  NO_SQ,
-  OFFBOARD
+  A1 = 21, B1, C1, D1, E1, F1, G1, H1,
+  A2 = 31, B2, C2, D2, E2, F2, G2, H2,
+  A3 = 41, B3, C3, D3, E3, F3, G3, H3,
+  A4 = 51, B4, C4, D4, E4, F4, G4, H4,
+  A5 = 61, B5, C5, D5, E5, F5, G5, H5,
+  A6 = 71, B6, C6, D6, E6, F6, G6, H6,
+  A7 = 81, B7, C7, D7, E7, F7, G7, H7,
+  A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ, OFFBOARD
 };
 
 enum { FALSE, TRUE };
@@ -136,103 +57,107 @@ enum { FALSE, TRUE };
 enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
 
 typedef struct {
-  int move;
-  int score;
+	int move;
+	int score;
 } S_MOVE;
 
 typedef struct {
-  S_MOVE moves[MAXPOSITIONMOVES];
-  int count;
+	S_MOVE moves[MAXPOSITIONMOVES];
+	int count;
 } S_MOVELIST;
 
-enum { HFNONE, HFALPHA, HFBETA, HFEXACT };
+enum {  HFNONE, HFALPHA, HFBETA, HFEXACT};
 
 typedef struct {
-  U64 posKey;
-  int move;
-  int score;
-  int depth;
-  int flags;
+	U64 posKey;
+	int move;
+	int score;
+	int depth;
+	int flags;
 } S_HASHENTRY;
 
 typedef struct {
-  S_HASHENTRY *pTable;
-  int numEntries;
-  int newWrite;
-  int overWrite;
-  int hit;
-  int cut;
+	S_HASHENTRY *pTable;
+	int numEntries;
+	int newWrite;
+	int overWrite;
+	int hit;
+	int cut;
 } S_HASHTABLE;
 
 typedef struct {
-  int move;
-  int castlePerm;
-  int enPas;
-  int fiftyMove;
-  U64 posKey;
+
+	int move;
+	int castlePerm;
+	int enPas;
+	int fiftyMove;
+	U64 posKey;
 
 } S_UNDO;
 
 typedef struct {
-  int pieces[BRD_SQ_NUM];
-  U64 pawns[3];
 
-  int KingSq[2];
+	int pieces[BRD_SQ_NUM];
+	U64 pawns[3];
 
-  int side;
-  int enPas;
-  int fiftyMove;
+	int KingSq[2];
 
-  int ply;
-  int hisPly;
+	int side;
+	int enPas;
+	int fiftyMove;
 
-  int castlePerm;
+	int ply;
+	int hisPly;
 
-  U64 posKey;
+	int castlePerm;
 
-  int pceNum[13];
-  int bigPce[2];
-  int majPce[2];
-  int minPce[2];
-  int material[2];
+	U64 posKey;
 
-  S_UNDO history[MAXGAMEMOVES];
+	int pceNum[13];
+	int bigPce[2];
+	int majPce[2];
+	int minPce[2];
+	int material[2];
 
-  // piece list
-  int pList[13][10];
+	S_UNDO history[MAXGAMEMOVES];
 
-  S_HASHTABLE HashTable[1];
-  int PvArray[MAXDEPTH];
+	// piece list
+	int pList[13][10];
 
-  int searchHistory[13][BRD_SQ_NUM];
-  int searchKillers[2][MAXDEPTH];
+	S_HASHTABLE HashTable[1];
+	int PvArray[MAXDEPTH];
+
+	int searchHistory[13][BRD_SQ_NUM];
+	int searchKillers[2][MAXDEPTH];
 
 } S_BOARD;
 
 typedef struct {
-  int starttime;
-  int stoptime;
-  int depth;
-  int timeset;
-  int movestogo;
 
-  long nodes;
+	int starttime;
+	int stoptime;
+	int depth;
+	int timeset;
+	int movestogo;
 
-  int quit;
-  int stopped;
+	long nodes;
 
-  float fh;
-  float fhf;
-  int nullCut;
+	int quit;
+	int stopped;
 
-  int GAME_MODE;
-  int POST_THINKING;
+	float fh;
+	float fhf;
+	int nullCut;
+
+	int GAME_MODE;
+	int POST_THINKING;
 
 } S_SEARCHINFO;
 
 typedef struct {
-  int UseBook;
+	int UseBook;
 } S_OPTIONS;
+
 
 /* GAME MOVE */
 
@@ -246,10 +171,10 @@ typedef struct {
 0001 0000 0000 0000 0000 0000 0000 -> Castle 0x1000000
 */
 
-#define FROMSQ(m) ((m)&0x7F)
-#define TOSQ(m) (((m) >> 7) & 0x7F)
-#define CAPTURED(m) (((m) >> 14) & 0xF)
-#define PROMOTED(m) (((m) >> 20) & 0xF)
+#define FROMSQ(m) ((m) & 0x7F)
+#define TOSQ(m) (((m)>>7) & 0x7F)
+#define CAPTURED(m) (((m)>>14) & 0xF)
+#define PROMOTED(m) (((m)>>20) & 0xF)
 
 #define MFLAGEP 0x40000
 #define MFLAGPS 0x80000
@@ -260,15 +185,16 @@ typedef struct {
 
 #define NOMOVE 0
 
+
 /* MACROS */
 
-#define FR2SQ(f, r) ((21 + (f)) + ((r)*10))
+#define FR2SQ(f,r) ( (21 + (f) ) + ( (r) * 10 ) )
 #define SQ64(sq120) (Sq120ToSq64[(sq120)])
 #define SQ120(sq64) (Sq64ToSq120[(sq64)])
 #define POP(b) PopBit(b)
 #define CNT(b) CountBits(b)
-#define CLRBIT(bb, sq) ((bb) &= ClearMask[(sq)])
-#define SETBIT(bb, sq) ((bb) |= SetMask[(sq)])
+#define CLRBIT(bb,sq) ((bb) &= ClearMask[(sq)])
+#define SETBIT(bb,sq) ((bb) |= SetMask[(sq)])
 
 #define IsBQ(p) (PieceBishopQueen[(p)])
 #define IsRQ(p) (PieceRookQueen[(p)])
@@ -348,7 +274,8 @@ extern char *PrSq(const int sq);
 extern void PrintMoveList(const S_MOVELIST *list);
 extern int ParseMove(char *ptrChar, S_BOARD *pos);
 
-// validate.c
+
+//validate.c
 extern int SqOnBoard(const int sq);
 extern int SideValid(const int side);
 extern int FileRankValid(const int fr);
@@ -357,7 +284,7 @@ extern int PieceValid(const int pce);
 extern void MirrorEvalTest(S_BOARD *pos);
 extern int SqIs120(const int sq);
 extern int PceValidEmptyOffbrd(const int pce);
-extern int MoveListOk(const S_MOVELIST *list, const S_BOARD *pos);
+extern int MoveListOk(const S_MOVELIST *list,  const S_BOARD *pos);
 extern void DebugAnalysisTest(S_BOARD *pos, S_SEARCHINFO *info);
 
 // movegen.c
@@ -384,17 +311,15 @@ extern void ReadInput(S_SEARCHINFO *info);
 
 // pvtable.c
 extern void InitHashTable(S_HASHTABLE *table, const int MB);
-extern void StoreHashEntry(S_BOARD *pos, const int move, int score,
-                           const int flags, const int depth);
-extern int ProbeHashEntry(S_BOARD *pos, int *move, int *score, int alpha,
-                          int beta, int depth);
+extern void StoreHashEntry(S_BOARD *pos, const int move, int score, const int flags, const int depth);
+extern int ProbeHashEntry(S_BOARD *pos, int *move, int *score, int alpha, int beta, int depth);
 extern int ProbePvMove(const S_BOARD *pos);
 extern int GetPvLine(const int depth, S_BOARD *pos);
 extern void ClearHashTable(S_HASHTABLE *table);
 
 // evaluate.c
 extern int EvalPosition(const S_BOARD *pos);
-extern void MirrorEvalTest(S_BOARD *pos);
+extern void MirrorEvalTest(S_BOARD *pos) ;
 
 // uci.c
 extern void Uci_Loop(S_BOARD *pos, S_SEARCHINFO *info);
@@ -406,6 +331,46 @@ extern void Console_Loop(S_BOARD *pos, S_SEARCHINFO *info);
 // polybook.c
 extern int GetBookMove(S_BOARD *board);
 extern void CleanPolyBook();
-extern void InitPolyBook();
+extern void InitPolyBook() ;
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	gcc xboard.c engine-nott.c uci.c evaluate.c pvtable.c init.c bitboard.c hashkeys.c board.c data.c attack.c io.c movegen.c validate.c makemove.c perft.c search.c misc.c polybook.c polykeys.c -o nottachessengine -O2
+	gcc xboard.c nottachessengine.c uci.c evaluate.c pvtable.c init.c bitboards.c hashkeys.c board.c data.c attack.c io.c movegen.c validate.c makemove.c perft.c search.c misc.c polybook.c polykeys.c -o nottachessengine -O2
